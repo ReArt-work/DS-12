dd0ee51e18968bb42cbf21bd860ceaf9f1c18b52
ReArt <67474804+ReArt-work@users.noreply.github.com>
2022-05-10 11:13:06 +0300

    Create Homework-02.py

---
+++ Lesson2/Homework-02.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+# -- Sheet --
+
+# Завдання 1
+
+a = int(input("Введіть число A: "))
+b = int(input("Введіть число B: "))
+a > 0
+a%2 == 0
+a//13 == 0
+a%2 and b%2 is not True
+a//2 == b//2
+
+# Завдання 2
+pswrd = input("Введіть пароль")
+(len(pswrd) > 7) and ("@"  in pswrd or "#" in pswrd)
+
+# Завдання 3
+char = input("Введіть символ Char")
+print(chr(ord(char)-1)," and ",chr(ord(char)+1))
+
+# Завдання 4
+input_string = input("Введіть фразу") 
+print(input_string[::-1])
+
